65837efa767c30baa58a05dde7a965cecf3e8f41
Badr El Abdellaoui <b.elabdellaoui101@gmail.com>
2022-08-16 13:11:59 +0100

    Added game logic. A function to compare the computer and user score. And a function to detect if the user got a BJ or if he busted.

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ computer_cards = []
 def deal_card():
                 return cards[random.randint(1,12)]
         
-#user_cards[i] == 11 and
 def calculate_score(card_list):
         for i in range(len(card_list)):
                 if  card_list[i] == 11 and sum(card_list) > 21:
@@ -25,16 +24,37 @@ def calculate_score(card_list):
                 return sum(card_list)
 
 def bj_bust():
+        global end
         if calculate_score(user_cards) == 0:
                 print("Congratulations. You just got a BLACKJACK. You win!")
+                end = True
         elif calculate_score(user_cards) > 21:
                 print("BUST. You went over 21. You lose, GAME OVER.")
-def play_again():
-        print("Do you want to play again?")
-        if choice.strip().lower() == "yes":
-                pass #main()
-        elif choice.strip().lower() == "no":
-                pass #quit
+                end = True
+
+def compare(user_score, computer_score):
+        if user_score == computer_score:
+                print("It's a draw.")
+        elif calculate_score(user_cards) == 0:
+                print("BLACKJACK. You win.")
+        elif user_score > 21:
+                print("You lose. Computer wins.")
+        elif computer_score > 21:
+                print("Computer loses. You win.")
+        else:
+                if user_score > computer_score:
+                        print("Computer loses. You win.")
+                else:
+                       print("You lose. Computer wins.")
+        global end
+        end = True
+
+# def play_again():
+#         print("Do you want to play again?")
+#         if choice.strip().lower() == "yes":
+#                 pass #main()
+#         elif choice.strip().lower() == "no":
+#                 pass #quit
 
 user_cards.append(deal_card())
 user_cards.append(deal_card())
@@ -56,3 +76,14 @@ choice = input("---> ")
 
 if choice.lower().strip() == "hit":
         user_cards.append(deal_card())
+        print("Here are your cards:", user_cards, "  |   Here is your score:", calculate_score(user_cards))
+        print("Here is the dealer's first card:", computer_cards[0])
+        bj_bust()
+        choice = input("---> ")
+
+if choice.lower().strip() == "stick":
+        print("Your final hand:", user_cards, "Final score:", calculate_score(user_cards))
+        while calculate_score(computer_cards) < 17:
+                computer_cards.append(deal_card())
+        print("Computer's final hand:", computer_cards, "Computer's final socre:", calculate_score(computer_cards))
+        compare(calculate_score(user_cards), calculate_score(computer_cards))
